15ed06b8d84da4a7fbd00a3fe6eade6253f64440
devofy <artemijv65@gmail.com>
2023-12-15 13:50:20 +0500

    Поправил фрактал

---
+++ Lesson4/Lesson4/Fractal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics;
+
+public enum Direction
+{
+    West,
+    South,
+    East,
+    North
+}
+namespace Lesson4
+{
+    public class Fractal
+    {
+        public void Draw( float x, float y,float size, int n, Direction dir)
+        {
+            //DrawSquare(0,0,2);
+            int n1 = n - 1;
+            float fr = 0.4f * size, cr = 1.4f * size;
+            if (n > 0)
+            {
+                Draw(x + cr, y, fr, n1, Direction.East);
+                Draw(x, y + cr, fr, n1, Direction.North);
+                Draw(x - cr, y, fr, n1, Direction.West);
+                Draw(x, y - cr, fr, n1, Direction.South);
+                DrawSquare(x,y,size);
+            }
+        }
+
+        private void DrawSquare(float x, float y, float size)
+        {
+            GL.Begin(BeginMode.Quads);
+            var z = 10;
+            float xA, xB, xC, xD, yA, yB, yC, yD;
+            xA = x - size;
+            yA = y - size;
+            xB = x + size;
+            yB = y - size;
+            xC = x + size;
+            yC = y + size;
+            xD = x - size;
+            yD = y + size;
+            GL.Vertex3(xA, yA, z);
+            GL.Vertex3(xB, yB, z);
+            GL.Vertex3(xC, yC, z);
+            GL.Vertex3(xD, yD, z);
+            GL.End();
+        }
+    }
+}
---
+++ Lesson4/Lesson4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+
+namespace Lesson4
+{
+    internal class Game : GameWindow
+    {
+        public Game() : base(1024, 1024, GraphicsMode.Default, "Open TK sample")
+        {
+            VSync = VSyncMode.On;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            GL.ClearColor(0.1f, 0.2f, 0.5f, 0.0f);
+            GL.Enable(EnableCap.DepthTest);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            var projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4,
+                Width / (float)Height, 1.0f, 64.0f);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projection);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            if (Keyboard[Key.Escape])
+                Exit();
+        }
+
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            var modelview = Matrix4.LookAt(Vector3.Zero, Vector3.UnitZ, Vector3.UnitY);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref modelview);
+            new Fractal().Draw(0,0,1,10, Direction.East);
+            SwapBuffers();
+        }
+
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            using (var game = new Game())
+            {
+                game.Run(30);
+            }
+        }
+    }
+}
